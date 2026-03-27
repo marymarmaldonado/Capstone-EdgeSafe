29d080df19bb2d7e81059af37fb09cc75368ab8e
JorgeFMM <jorge.marin5@upr.edu>
2026-03-27 18:01:03 -0400

    implement logger

---
+++ backend/services/logger.py
@@ -0,0 +1,13 @@
+from backend.database.db import get_db
+
+def log_event(model_name, inference_ms, timestamp, confidence, detected, source, image_path):
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO detection_events (model_name, inference_ms, timestamp, confidence, detected, source, image_path)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (model_name, inference_ms, timestamp, confidence, detected, source, image_path))
+
+    conn.commit()
+    conn.close()
