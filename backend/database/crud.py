ced7bb87bdc62e3ee5e98f8580af3e700135e86a
JorgeFMM <jorge.marin5@upr.edu>
2026-03-28 18:17:50 -0400

    implement get_all_events API route

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -1,4 +1,4 @@
-from backend.database.db import get_db
+from database.db import get_db
 
 def insert_detection_event(model_name, inference_ms, timestamp, confidence, detected, source, image_path):
     conn = get_db()
@@ -11,3 +11,15 @@ def insert_detection_event(model_name, inference_ms, timestamp, confidence, dete
 
     conn.commit()
     conn.close()
+
+
+def get_all_events():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM detection_events")
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return rows
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from database.crud import get_all_events
 
 app = FastAPI()
+
 
 # Root route (test if API works)
 # To test, run "uvicorn main:app --reload" in the backend folder
 @app.get("/")
 def root():
     return {"message": "EdgeSafe API running"}
+
+
+@app.get("/events")
+def read_events():
+    events = get_all_events()
+    return [dict(e) for e in events]
+
