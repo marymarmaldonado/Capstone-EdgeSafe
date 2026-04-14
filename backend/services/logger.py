a8519c9cd3d4684f121270a5032be0e2ca232597
jorgefm <jorge.marin5@upr.edu>
2026-04-14 16:33:55 -0400

    fix detection condition in if statement

--- backend/services/logger.py
+++ backend/services/logger.py
@@ -1,7 +1,7 @@
 from backend.database.crud import insert_detection_event
 
 def log_event(model_name, inference_ms, timestamp, confidence, detected, source, image_path):
-    if model_name and inference_ms and timestamp and confidence and detected and source and image_path:
+    if model_name and inference_ms and timestamp and confidence and (detected is not None) and source and image_path:
         insert_detection_event(model_name, inference_ms, timestamp, confidence, detected, source, image_path)
 
     else:
